2bde94c8cfbd35c44e7212b132d698c10c1c0138
expert-med <contato@expertmed.tech>
2024-03-21 15:04:04 -0300

    main

--- lib/pages/corretor_clientes/corretor_info_page.dart
+++ lib/pages/corretor_clientes/corretor_info_page.dart
@@ -4,7 +4,7 @@ import 'package:projeto_imobiliaria/util/app_bar_model.dart';
 import '../../components/imovel/imovel_grid_favorites.dart';
 import '../../models/corretores/corretor.dart';
 import '../../util/new_app_bar_model.dart';
-import '../corretores/lista_corretores_page.dart';
+
 import '../imoveis/imoveis_Favoritos.dart';
 import '../map/map_page.dart';
 
